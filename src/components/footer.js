aa0ff776d654f142860787c27b2f2699b6712145
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-10 21:34:05 -0400

    added image gallery for sample lesson

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,35 +6,35 @@ import NaverBlog from '../assets/naverBlog.png';
 
 export default function Footer() {
     return (
-        <div className="h-72 w-full bg-gray-600 text-white flex flex-row px-32 py-16 text-base">
+        <div className="min-h-72 h-auto w-full bg-gray-600 text-white flex flex-col space-y-12 sm:space-y-0 sm:flex-row px-4 md:px-32 py-16 text-base">
             {/* left side with name and short bio */}
-            <div className="flex flex-col space-y-8 w-1/2 h-full">
-                <h1 className="text-3xl"> 제니구의 영어전담마크 </h1>
-                <p> 당신을 위한 1:1 온라인 영어강의 </p>
-                <div className="flex flex-col space-y-2">
+            <div className="flex flex-col space-y-8 w-full sm:w-1/2 h-full">
+                <h1 className="text-2xl md:text-3xl"> 제니구의 영어전담마크 </h1>
+                <p className="text-sm md:text-base"> 당신을 위한 1:1 온라인 영어강의 </p>
+                <div className="flex flex-col space-y-2 text-sm md:text-base">
                     <p className=""> 이메일: </p>
                     <p className=""> 전화번호: </p>
                 </div>
 
             </div>
             {/* right side with socials */}
-            <div className="flex flex-col space-y-6 w-1/2 h-full">
+            <div className="flex flex-col space-y-4 md:space-y-6 w-1/2 h-full">
                 <div>
                     <a href="https://www.youtube.com/@jenny9english" target="_blank" rel="noopener noreferrer" className="flex items-center flex-row space-x-4">
                         <Image src = {Youtube} alt = 'Youtube Icon' className="w-8 h-8"/>
-                        <p className="transition duration-300 ease-in-out hover:text-pink-200"> 제니구의 영어전담마크 </p>
+                        <p className="text-sm md:text-base transition duration-300 ease-in-out hover:text-pink-200"> 제니구의 영어전담마크 </p>
                     </a>
                 </div>
                 <div>
                     <a href="https://www.instagram.com/jenny9_english" target="_blank" rel="noopener noreferrer" className="flex items-center flex-row space-x-4">
                         <Image src = {Instagram} alt = 'Instagram Icon' className="w-8 h-8"/>
-                        <p className="transition duration-300 ease-in-out hover:text-pink-200"> jenny9_english </p>
+                        <p className="text-sm md:text-base transition duration-300 ease-in-out hover:text-pink-200"> jenny9_english </p>
                     </a>
                 </div>
                 <div>
                     <a href="https://blog.naver.com/needyous2" target="_blank" rel="noopener noreferrer" className="flex items-center flex-row space-x-4">
                         <Image src = {NaverBlog} alt = 'Naver Blog Icon' className="w-8 h-8"/>
-                        <p className="transition duration-300 ease-in-out hover:text-pink-200"> 친절한 제니구 </p>
+                        <p className="text-sm md:text-base transition duration-300 ease-in-out hover:text-pink-200"> 친절한 제니구 </p>
                     </a>
                 </div>
 
--- src/pages/programs.js
+++ src/pages/programs.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import Image from 'next/image';
 import banner from '@assets/programs/banner.jpg'
+import ImageGallery from "react-image-gallery";
+import sample1 from "@assets/programs/sample1.png";
+import sample2 from "@assets/programs/sample2.png";
+import sample3 from "@assets/programs/sample3.png";
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 export default function Programs() {
+
+    const images = [
+        {
+          original: sample1.src,
+        },
+        {
+          original: sample2.src,
+        },
+        {
+          original: sample3.src,
+        },
+      ];
+
     return (
         <div className='min-h-screen h-auto flex flex-col items-center'>
             <Image src={banner.src} alt='banner' layout="responsive" width={16} height={9}/>
             <iframe className='w-full h-[60vh]' src="https://www.youtube.com/embed/H2mogYDmMPs?autoplay=1&si=pIkr77biVv_pe65c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen />
-            <div className='mt-12 w-full h-2/5 flex items-center justify-center'>
+            <div className='mt-12 w-full h-2/5 flex flex-col space-y-8 items-center justify-center py-20'>
                 <h1 className='text-4xl font-bold'> 샘플 레슨 </h1>
-                
+                <ImageGallery items={images} showThumbnails={false} autoPlay={true}/>
             </div>
         </div>
     );
